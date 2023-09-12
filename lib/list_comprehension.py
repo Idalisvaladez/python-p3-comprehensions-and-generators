056d56e9fa743d1e0002794230c7ba1872898931
Idalis Valadez <idalisvaladez22@gmail.com>
2023-09-12 18:06:10 -0500

    Complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [num for num in num_list if num % 2 == False]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    excited_sentence = [sentence + "!" for sentence in sentence_list]
+    return excited_sentence
